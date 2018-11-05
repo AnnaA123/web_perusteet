66bec6b5a057decdd0ffc90220c659fbab16b1b4
Annaa <32773252+AnnaA123@users.noreply.github.com>
2018-11-05 11:54:48 +0200

    Update teht5.js

--- DOM-tehtavat-master/js/teht5.js
+++ DOM-tehtavat-master/js/teht5.js
@@ -1,7 +1,13 @@
+function addZero(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
 
 function aika() {
-  //let klo = new Date();
-  document.querySelector('#kello').innerHTML = '' + new Date() + '';
+  let day = new Date();
+  document.querySelector('#kello').innerHTML = '' + addZero(day.getHours()) + ':' + addZero(day.getMinutes()) + ':' + addZero(day.getSeconds());
 }
 
 setInterval(aika, 1000);
